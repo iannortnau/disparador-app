f6c9cb553d81926acf82af294b271b13e2ededfa
Iann Ortnau <iann.santos@e-unicred.com.br>
2022-09-29 21:10:50 -0300

    fix: build win

--- main/whatsClient.js
+++ main/whatsClient.js
@@ -5,7 +5,7 @@ let chromeRoute;
 if(sist === "linux"){
   chromeRoute = "/usr/bin/google-chrome-stable";
 }else {
-  chromeRoute = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+  chromeRoute = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
 }
 const client = new Client({
   authStrategy: new LocalAuth({clientId: "client-Chrome",dataPath:"./"}),
